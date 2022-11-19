fa6c3f89d348a192dfd0a84d7c692581a598d30d
DiogoAMV <diogo.amv19@gmail.com>
2022-11-18 23:40:09 -0300

    :tada: api finished

--- api/src/app/useCases/orders/createOrder.ts
+++ api/src/app/useCases/orders/createOrder.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { io } from '../../..';
 
 import { Order } from '../../models/Order';
 
@@ -9,7 +10,9 @@ export async function createOrder(req: Request, res: Response) {
     const order = await Order.create({
       table, products
     });
+    const orderDetails = order.populate('products.product');
 
+    io.emit('orders@new', order);
     res.status(201).json(order);
   } catch (error) {
     console.log(error);
