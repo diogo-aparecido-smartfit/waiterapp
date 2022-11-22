294a209fa719d8dc812599ffce48d2e384b19140
DiogoAMV <diogo.amv19@gmail.com>
2022-11-22 19:02:02 -0300

    :tada: mobile application

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
-import { categories } from '../../mocks/categories';
 import { Category } from '../../types/Category';
 import { Text } from '../Text';
 
--- mobile/src/utils/api.ts
+++ mobile/src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.10:3333',
+  baseURL: 'http://172.26.107.193:3333',
 });
